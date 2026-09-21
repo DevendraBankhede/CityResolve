afee3d34a814e5bf473565c82312eb32211869a1
HodManagmentSystem <harshubankhede@gmail.com>
2026-09-21 15:24:22 +0530

    feat: add Register component with form handling and role selection

---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,225 @@
+import React, { useState } from "react";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import {
+  Building2,
+  Eye,
+  EyeOff,
+  Lock,
+  Mail,
+  User,
+  UserCheck,
+  ShieldCheck,
+} from "lucide-react";
+import toast from "react-hot-toast";
+import { useAuth } from "../context/AuthContext";
+
+const Register = () => {
+  const { register } = useAuth();
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    role: "user",
+  });
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handelSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.name || !formData.email || !formData.password) {
+      toast.error("Please fill in all required fields.");
+      return;
+    }
+
+    if (formData.password !== formData.confirmPassword) {
+      toast.error("Password do not match! ");
+      return;
+    }
+    try {
+      setLoading(true);
+      const res = await register({
+        name: formData.name,
+        email: formData.email,
+        password: formData.password,
+        role: formData.role,
+      });
+
+      if (res.success) {
+        toast.success("Account Created Successfully");
+        const redirectPath =
+          formData.role === "admin" ? "/admin" : "/dashboard";
+        navigate(redirectPath);
+      } else {
+        toast.error(req.message || "Registeration failed");
+      }
+    } catch (err) {
+      toast.error(
+        err.response?.data?.message || "Registration error occurred.",
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <div className="min-h-screen bg-state-50 flex flex-col font-sans">
+      <div className="flex-1 flex flex-col justify-center py-8 sm:py-12 px-4 sm:px-6 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md text-center space-y-3 mb-6">
+          <Link className="inline-flex items-center justify-center group">
+            <div className="w-12 h-12 bg-[#0088cc] rounded-2xl flex items-center justify-center text-white shadow-lg shadow-sky-600/20 group-hover:scale-105 transition-transform duration-200">
+              <Building2 className="w-6 h-6 stroke-[2.2]" />
+            </div>
+          </Link>
+          <div>
+            <h2 className="text-2xl font-black text-slate-900 tracking-tight">
+              Create your CityResolve Account
+            </h2>
+            <p className="text-xs text-slate-500 mt-1 font-medium">
+              join thousands of citizens marking cities smarter
+            </p>
+          </div>
+        </div>
+
+        <div className="sm:mx-auto sm:max-w-md">
+          <div className="bg-white py-8 px-6 sm:px-10 shadow-xl shadow-slate-200/50 rounded-3xl border border-slate-200/80">
+            <form onSubmit={handelSubmit} className="space-y-4">
+              {/*Role Selection */}
+              <div>
+                <label className="block text-xs font-bold text-slate-700 mb-2">
+                  Account Role
+                </label>
+                <div className="grid p-1bg-slate-100 rounded-xl">
+                  <button
+                    type="button"
+                    onClick={() => setFormData({ ...formData, role: "user" })}
+                    className={`flex items-center justify-center gap-2 py-2 px-3 rounded-lg text-xs font-bold transition ${
+                      formData.role === "user"
+                        ? "bg-white text-blue-600 shadow-sm"
+                        : "text-slate-800"
+                    }`}
+                  >
+                    <UserCheck className="w-4 h-4" />
+                    <span>CitZen</span>
+                  </button>
+                </div>
+              </div>
+
+              {/* Name */}
+              <div>
+                <label className="block text-xs font-bold text-slate-700 mb-1">
+                  Full Name
+                </label>
+                <div className="relative">
+                  <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none text-slate-400">
+                    <User className="w-4 h-4" />
+                  </div>
+                  <input
+                    type="text"
+                    name="name"
+                    value={formData.name}
+                    placeholder="Rahul Sharma"
+                    className="w-full pl-10 pr-4 py-2.5 bg-slate-50 border border-slate-200 rounded-xl text-xs font-medium text-slate-900 focus:bg-white focus:outline-none focus:ring-2 focus:ring-blue-500 transition"
+                    required
+                  />
+                </div>
+              </div>
+
+              {/* Email */}
+              <div>
+                <label className="block text-xs font-bold text-slate-700 mb-1">
+                  Email Address
+                </label>
+                <div className="relative">
+                  <div className="absolute inset-y-0 left-0 pl-3 flex item-center pointer-event-none text-slate-400">
+                    <Mail className="w-4 h-4" />
+                  </div>
+                  <input
+                    type="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    placeholder="name@example.com"
+                    className="w-full pl-10 pr-4 py-2.5 bg-slate-50 border border-slate-200 rounded-xl text-xs font-medium text-slate-900 focus:outline-none focus:ring-2 focus:ring-blue-500 transition"
+                    required
+                  />
+                </div>
+              </div>
+
+              {/*Password */}
+              <div>
+                <label className="block text-xs font-bold text-slate-700 mb-1">
+                  Password
+                </label>
+                <div className="relative">
+                  <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none text-slate-400">
+                    <Lock className="w-4 h-4" />{" "}
+                  </div>
+                </div>
+                <input
+                  type={showPassword ? "text" : "password"}
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  placeholder="......."
+                  className="w-full pl-10 pr-10 p-2.5 bg-slate-50 border-slate-200 rounded-xl text-xl font-medium text-slate-900 focus:bg-white focus:outline-none focus:-ring-2 focus:-ring-blue-500 transition"
+                  required
+                />
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="absolute inset-y-0 right-0 pr-3 flex items-center text-slate-400"
+                >
+                  {showPassword ? (
+                    <EyeOff className="w-4 h-4" />
+                  ) : (
+                    <Eye className="w-4 h-4" />
+                  )}
+                </button>
+              </div>
+
+              {/* Confirm Password */}
+              <div>
+                <label className="block text-xs font-bold text-slate-700 mb-1">Confirm Password</label>
+                <div className="relative">
+                  <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none text-slate-400">
+                    <Lock className="w-4 h-4" />
+                  </div>
+                  <input type="password"
+                          name="confirmPassword"
+                          value={formData.confirmPassword}
+                          onChange={handleChange} 
+                          placeholder="......."
+                          className="w-full pl-10 pr-4 py-2.5 bg-slate-50 border border-slate-200 rounded-xl text-xs font-medium text-slate-900  focus:bg-white focus:outline-none focus:ring-2 focus:ring-blue-500 transition"
+                          required
+                          />
+                </div>
+              </div>
+
+              <button type="submit" 
+                      disabled={loading}
+                      className="w-full py-3 bg-blue-600 hover:bg-blue-700 text-white font-bold text-xs rounded-xl shadow-lg shadow-blue-600/30 transition flex items-center justify-center gap-2 disabled:opacity-50"
+              >
+                {loading ? "Creating Account..." : "Create Account"}
+              </button>
+            </form>
+
+            <div className="mt-6 text-center text-xs text-slate-600">
+              Already have an account?{" "}
+              <Link className="font-bold text-blue-600 hover:underline">
+              Sing in
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Register;
